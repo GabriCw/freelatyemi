df9e124edab3b7cd57193e288ce9040ba548be6d
Gabriel <gabricouto2305@gmail.com>
2023-10-05 21:06:30 -0300

    Falta integrar nova conta e pagar conta

--- src/view/home.py
+++ src/view/home.py
@@ -5,13 +5,17 @@ from ..control.main import Projeto
 # Define global variables to store the windows
 janela = None
 janela2 = None
+janela3 = None
+janela4 = None
 
 def abrir_janela1():
     global janela
     global janela2
+    global janela3
+    global janela4
 
     if janela2:
-        janela2.destroy()
+        janela2.withdraw()
 
     janela = customtkinter.CTk()
     janela.title("Splitwise")
@@ -67,9 +71,17 @@ def abrir_janela1():
 def abrir_janela2():
     global janela
     global janela2
+    global janela3
+    global janela4
 
     if janela:
-        janela.destroy()
+        janela.withdraw()
+    
+    if janela3:
+        janela3.withdraw()
+
+    if janela4:
+        janela4.withdraw()
 
     janela2 = customtkinter.CTk()
     janela2.title("Splitwise")
@@ -77,7 +89,7 @@ def abrir_janela2():
     janela2.maxsize(width=500, height=700)
 
     # Button to go back to janela1
-    botao2 = customtkinter.CTkButton(janela2, text="Voltar", command=lambda: abrir_janela1())
+    botao2 = customtkinter.CTkButton(janela2, text="Voltar", command=abrir_janela1)
     botao2.place(x=20, y=20)
     botao2.configure(corner_radius=10, font=("Arial",15, "bold"))
 
@@ -95,19 +107,125 @@ def abrir_janela2():
 
     nomes = customtkinter.CTkLabel(janela2, text=transactions_string)
     nomes.pack(pady=20, side=TOP)
-    print("Transactions string:", transactions_string)
+    nomes.configure(font=("Arial",16, "bold"))
 
     # Button to go back to janela1
-    botao3 = customtkinter.CTkButton(janela2, text="Nova Conta")
+    botao3 = customtkinter.CTkButton(janela2, text="Nova Conta", command=abrir_janela3)
     botao3.pack(padx=70, pady=5, side=LEFT)
     botao3.configure(corner_radius=10, font=("Arial",15, "bold"))
 
     # Button to go back to janela1
-    botao4 = customtkinter.CTkButton(janela2, text="Pagar Conta")
+    botao4 = customtkinter.CTkButton(janela2, text="Pagar Conta", command=abrir_janela4)
     botao4.pack(padx=10, pady=5, side=LEFT)
     botao4.configure(corner_radius=10, font=("Arial",15, "bold"))
 
     janela2.mainloop()
+
+def abrir_janela3():
+
+    global janela
+    global janela2
+    global janela3
+    global janela4
+
+    if janela2:
+        janela2.withdraw()
+
+    janela3 = customtkinter.CTk()
+    janela3.title("Splitwise")
+    janela3.geometry("500x700")
+    janela3.maxsize(width=500, height=700)
+
+    # Button to go back to janela2
+    botao4 = customtkinter.CTkButton(janela3, text="Voltar", command=abrir_janela2)
+    botao4.place(x=20, y=20)
+    botao4.configure(corner_radius=10, font=("Arial",15, "bold"))
+
+    #MARGEM TOP
+    margem_cima = customtkinter.CTkLabel(janela3, text="")
+    margem_cima.pack(pady=50, side=TOP)
+
+    # LABEL PAGADOR
+    nome_pagador = customtkinter.CTkLabel(janela3, text="Nome do Pagador:", font=("Arial",16, "bold"))
+    nome_pagador.pack(padx=10, pady=3)
+
+    # ENTRY PAGADOR
+    entry2 = customtkinter.CTkEntry(janela3)
+    entry2.pack(padx=10, pady=0)
+    entry2.configure(fg_color="#3d3d3d")
+
+    # LABEL PREÇO
+    preco = customtkinter.CTkLabel(janela3, text="Valor da Conta:     ", font=("Arial",16, "bold"))
+    preco.pack(padx=10, pady=3)
+
+    # ENTRY PREÇO
+    entry3 = customtkinter.CTkEntry(janela3)
+    entry3.pack(padx=10, pady=0)
+    entry3.configure(fg_color="#3d3d3d")
+
+    #MARGEM
+    margem2 = customtkinter.CTkLabel(janela3, text="")
+    margem2.pack(pady=20, side=TOP)
+
+    # BOTÃO ADICIONAR CONTA
+    botao5 = customtkinter.CTkButton(janela3, text="Adicionar Conta")
+    botao5.pack(padx=10, pady=10)
+    botao5.configure(corner_radius=10, font=("Arial",15, "bold"))
+
+    janela3.mainloop()
+
+def abrir_janela4():
+
+    global janela
+    global janela2
+    global janela3
+    global janela4
+
+    if janela2:
+        janela2.withdraw()
+
+    janela4 = customtkinter.CTk()
+    janela4.title("Splitwise")
+    janela4.geometry("500x700")
+    janela4.maxsize(width=500, height=700)
+
+    # Button to go back to janela2
+    botao6 = customtkinter.CTkButton(janela4, text="Voltar", command=abrir_janela2)
+    botao6.place(x=20, y=20)
+    botao6.configure(corner_radius=10, font=("Arial",15, "bold"))
+
+    #MARGEM TOP
+    margem_cima = customtkinter.CTkLabel(janela4, text="")
+    margem_cima.pack(pady=50, side=TOP)
+
+    # LABEL PAGADOR
+    nome_pagador2 = customtkinter.CTkLabel(janela4, text="Nome do Pagador:", font=("Arial",16, "bold"))
+    nome_pagador2.pack(padx=10, pady=3)
+
+    # ENTRY PAGADOR
+    entry4 = customtkinter.CTkEntry(janela4)
+    entry4.pack(padx=10, pady=0)
+    entry4.configure(fg_color="#3d3d3d")
+
+    # LABEL BENEFICIADO
+    nome_beneficiado = customtkinter.CTkLabel(janela4, text="Nome do Beneficiado:", font=("Arial",16, "bold"))
+    nome_beneficiado.pack(padx=10, pady=3)
+
+    # ENTRY PREÇO
+    entry5 = customtkinter.CTkEntry(janela4)
+    entry5.pack(padx=10, pady=0)
+    entry5.configure(fg_color="#3d3d3d")
+
+    #MARGEM
+    margem2 = customtkinter.CTkLabel(janela4, text="")
+    margem2.pack(pady=20, side=TOP)
+
+    # BOTÃO ADICIONAR CONTA
+    botao7 = customtkinter.CTkButton(janela4, text="Pagar Divida")
+    botao7.pack(padx=10, pady=10)
+    botao7.configure(corner_radius=10, font=("Arial",15, "bold"))
+
+    janela4.mainloop()
 
 # Run the initial window (janela1)
 users = [{'Gabriel': 5}, {'Felipe': 10}, {'Davi': 5}, {'Jonathan': 7}, {'Gabriel P': 10}, {'Matheus': 20}]
