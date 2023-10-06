27571661ded7d1f26cf0bc856839fbbd8b8fd048
Felipe_Silvieri <felipesilvieri@yahoo.com>
2023-10-06 03:13:26 -0300

    deletando arquivo de teste

--- src/controller/bla.py
+++
@@ -1,9 +0,0 @@
-class Projeto:
-    def __init__(self, users):
-        self.users = users
-
-users = [{'Gabriel': 5}, {'Felipe': 10}, {'Davi': 5}, {'Jonathan': 7}, {'Gabriel P': 10}, {'Matheus': 20}]
-
-projeto = Projeto(users)
-
-print(projeto.users)
